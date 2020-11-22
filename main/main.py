cd22f39d0c33e4281d9783fba5d3544829a7cc59
Pushyamitra Thammineni <pavan.pushyamitra99@gmail.com>
2020-11-22 18:37:41 +0530

    changed commands

--- main/main.py
+++ main/main.py
@@ -11,10 +11,23 @@ with sr.Microphone() as source:
     print("SPEAK NOW")
     try:
         recognised_text = rec(source)
-        if recognised_text == "hai":
-            a = "hello there!"
-            engine(a)
+        command = recognised_text.split(" ")
+        ''' Some basic commands that bot can respond
+            hi there! -> Bot introduce herself
+            Search (filename0 -> Bot searchs file or folder  in directory
+            play (songname) -> Bot plays locally stored music
+        '''
+        
+        if command[0] == 'hai':
+            greetings()
+            print("Speak Something..")
+            hai_respond = rec(source)
+        
+        elif command[0] == 'search':
+            searching(command[1])
+
     except sr.UnknownValueError:
         print("Unknownvalueerror")
+   
     except sr.RequestError:
         print("Requesterror")
